d46c291395985c2b7a3aaa5e2be1db0ac5829c6a
Alejo Boga <alejoboga20@gmail.com>
2022-01-12 10:52:56 -0300

    Configuring mapbox

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 import { MapApp } from './MapApp';
+
+mapboxgl.accessToken =
+	'pk.eyJ1IjoiYWxlam9ib2dhMjAiLCJhIjoiY2t5Ymxpcjd4MGdnbDJ1bWY5d3F6c2J1bSJ9.TCSWxE4QZztgbqoFVTFHXw';
 
 if (!navigator.geolocation) {
 	alert("Your browser doesn't have geolocation");
