888edeb8f5509303655ca050ffc482db40fd928a
Alejo Boga <alejoboga20@gmail.com>
2022-01-13 10:18:04 -0300

    Adding logo

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,12 @@
 import { BtnMyLocation, MapView } from '../components';
+import { ReactLogo } from '../components/ReactLogo';
 
 export const HomeScreen = () => {
 	return (
 		<div>
 			<MapView />
 			<BtnMyLocation />
+			<ReactLogo />
 		</div>
 	);
 };
