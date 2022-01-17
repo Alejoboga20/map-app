26f533243f1a54edfb7c32e26ad089e6154f0f60
Alejo Boga <alejoboga20@gmail.com>
2022-01-17 09:27:10 -0300

    Fly to selected place

--- src/components/SearchResult.tsx
+++ src/components/SearchResult.tsx
@@ -1,9 +1,22 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { LoadingPlaces } from '.';
-import { PlacesContext } from '../context';
+import { PlacesContext, MapContext } from '../context';
+import { Feature } from '../interfaces/places';
 
 export const SearchResult = () => {
+	const [activeId, setActiveId] = useState('');
 	const { places, isLoadingPlaces } = useContext(PlacesContext);
+	const { map } = useContext(MapContext);
+
+	const onPlaceClick = (place: Feature) => {
+		const [lng, lat] = place.center;
+		setActiveId(place.id);
+
+		map?.flyTo({
+			zoom: 14,
+			center: [lng, lat],
+		});
+	};
 
 	if (isLoadingPlaces) return <LoadingPlaces />;
 
@@ -14,12 +27,22 @@ export const SearchResult = () => {
 	return (
 		<ul className='list-group mt-3'>
 			{places.map((place) => (
-				<li key={place.id} className='list-group-item list-group-item-action'>
+				<li
+					key={place.id}
+					className={`list-group-item list-group-item-action pointer ${
+						activeId === place.id ? 'active' : ''
+					}`}
+					onClick={() => onPlaceClick(place)}
+				>
 					<h6>{place.text_es}</h6>
-					<p className='text-muted' style={{ fontSize: '12px' }}>
-						{place.place_name}
-					</p>
-					<button className='btn btn-outline-primary btn-sm'>Addresses</button>
+					<p style={{ fontSize: '12px' }}>{place.place_name}</p>
+					<button
+						className={`btn btn-sm ${
+							activeId === place.id ? 'btn-outline-light' : 'btn-outline-primary'
+						}`}
+					>
+						Addresses
+					</button>
 				</li>
 			))}
 		</ul>
