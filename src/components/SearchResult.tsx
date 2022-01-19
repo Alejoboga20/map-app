6993b6ea304be63ffa3eacfdd366f9e8f3fbc96f
Alejo Boga <alejoboga20@gmail.com>
2022-01-19 15:03:05 -0300

    Making list scrollable

--- src/components/SearchResult.tsx
+++ src/components/SearchResult.tsx
@@ -32,27 +32,29 @@ export const SearchResult = () => {
 	}
 
 	return (
-		<ul className='list-group mt-3'>
-			{places.map((place) => (
-				<li
-					key={place.id}
-					className={`list-group-item list-group-item-action pointer ${
-						activeId === place.id ? 'active' : ''
-					}`}
-					onClick={() => onPlaceClick(place)}
-				>
-					<h6>{place.text_es}</h6>
-					<p style={{ fontSize: '12px' }}>{place.place_name}</p>
-					<button
-						onClick={() => getRoute(place)}
-						className={`btn btn-sm ${
-							activeId === place.id ? 'btn-outline-light' : 'btn-outline-primary'
+		<div className='list-scrollable'>
+			<ul className='list-group mt-3 '>
+				{places.map((place) => (
+					<li
+						key={place.id}
+						className={`list-group-item list-group-item-action pointer ${
+							activeId === place.id ? 'active' : ''
 						}`}
+						onClick={() => onPlaceClick(place)}
 					>
-						Addresses
-					</button>
-				</li>
-			))}
-		</ul>
+						<h6>{place.text_es}</h6>
+						<p style={{ fontSize: '12px' }}>{place.place_name}</p>
+						<button
+							onClick={() => getRoute(place)}
+							className={`btn btn-sm ${
+								activeId === place.id ? 'btn-outline-light' : 'btn-outline-primary'
+							}`}
+						>
+							Addresses
+						</button>
+					</li>
+				))}
+			</ul>
+		</div>
 	);
 };
